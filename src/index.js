c44cb867adbd5d154ae8657d08eaccbac7cd2970
ArtsiomRusak <lestat47@tut.by>
2023-01-25 23:48:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = n + "";
+    if (n >= 0) {
+        return n.split("").reverse().join("");
+    } else {
+        return (n.split("").reverse().join("")).slice(0, -1);
+    }
 }
